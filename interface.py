2b3577f1d5a5a5175c0c3127a290116a53d14900
Guilherme Dias <guilhermeddealmeida@gmail.com>
2023-12-01 14:44:37 -0300

    Projeto atualizado

--- interface.py
+++ interface.py
@@ -1,27 +1,23 @@
-from doctest import master
-from msilib.schema import SelfReg
 import tkinter as tk
 from tkinter import messagebox
-from typing import Self
-from main import jogo 
+from main import jogo
 
 class JogoMatematicaGUI:
     def __init__(self, master):
-    Self.master = master
-    Self.master.title("Jogo de Matemática")
+        self.master = master
+        self.master.title("Jogo de Matemática")
 
-    Self.label = tk.Label(master, text="Bem-vindo ao Jogo de Matemática!")
-    Self.label.pack()
+        self.label = tk.Label(master, text="Bem-vindo ao Jogo de Matemática!")
+        self.label.pack()
 
-    Self.iniciar_button = tk.Button(master, text="Iniciar Jogo", command=SelfReg.iniciar_jogo)
-    Self.iniciar_button.pack()
+        self.iniciar_button = tk.Button(master, text="Iniciar Jogo", command=self.iniciar_jogo)
+        self.iniciar_button.pack()
 
     def iniciar_jogo(self):
         pontuacao = jogo()
         messagebox.showinfo(title="Fim do Jogo", message=f"Sua pontuação final é: {pontuacao}")
 
-        if __name__ == "__main__":
-            root = tk.Tk()
-            app = JogoMatematicaGUI(root)
-            root.mainloop()
-
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = JogoMatematicaGUI(root)
+    root.mainloop()
--- main.py
+++ main.py
@@ -1,38 +1,36 @@
-import random 
-
+import random
 
 def gera_pergunta():
     operador = random.choice(['+', '-'])
-    num1 = random.randit(1, 10)
-    num2 = random.randit(1, 10)
+    num1 = random.randint(1, 10)  # Correção: "randint" ao invés de "randit"
+    num2 = random.randint(1, 10)  # Correção: "randint" ao invés de "randit"
 
     if operador == '-':
-        # Certifica-se de que a subtração não resulta em números negativos
+        # Certifique-se de que a subtração não resulta em números negativos
         num1, num2 = max(num1, num2), min(num1, num2)
 
-        pergunta = f"{num1} {operador} {num2}"
-        resposta = eval(pergunta) # Avalia a expressão para obter a reposta
+    pergunta = f"{num1} {operador} {num2}"
+    resposta_correta = eval(pergunta)  # Correção: "resposta_correta" ao invés de "resposta"
 
-        return pergunta, resposta
+    return pergunta, resposta_correta
 
-        def jogo():
-            pontuacao = 0
-            num_perguntas = 10 # Número de perguntas a serem feitas       
-        print("Bem-vindo ao Jogo de Matemática!")
+def jogo():
+    pontuacao = 0
+    num_perguntas = 10  # Número de perguntas a serem feitas
+    print("Bem-vindo ao Jogo de Matemática!")
 
-        for _ in range(num_perguntas):
-            pergunta, resposta_correnta = gera_pergunta()
-            print(f"\nPergunta: {pergunta}")
-            resposta_usuario = int(input("Sua resposta: "
-            
-            ))
+    for _ in range(num_perguntas):
+        pergunta, resposta_correta = gera_pergunta()
+        print(f"\nPergunta: {pergunta}")
+        resposta_usuario = int(input("Sua resposta: "))
 
-            if resposta_usuario == resposta_correta:
-                print("Resposta correta! +1 ponto\n")
-                pontuacao += 1
-            else:
-                print("Resposta incorreta. A resposta correta era: {resposta_correta}\n")
-                print(f"\Fim do jogo! Sua pontuação final é: {pontuacao}")
+        if resposta_usuario == resposta_correta:
+            print("Resposta correta! +1 ponto\n")
+            pontuacao += 1
+        else:
+            print(f"Resposta incorreta. A resposta correta era: {resposta_correta}\n")
 
-                if __name__ == "__main__":
-                    jogo()
+    print(f"Fim do jogo! Sua pontuação final é: {pontuacao}")
+
+if __name__ == "__main__":
+    jogo()
